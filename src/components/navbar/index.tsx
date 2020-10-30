33ab6242bfe88b7411f7d5752cd4402d6178480e
LeandroViegas <l3androviegas@gmail.com>
2020-10-30 14:25:45 -0300

    1.0.4

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -63,7 +63,7 @@ class Navbar extends React.Component<MyProps, MyState> {
                 } else {
                     Api.get("/auth/logout")
                     localStorage.removeItem("user")
-                    // document.location.reload(true)
+                    document.location.reload(true)
                 }
             })
     }
@@ -107,12 +107,12 @@ class Navbar extends React.Component<MyProps, MyState> {
             if (!results[2]) return '';
             return decodeURIComponent(results[2].replace(/\+/g, ' '));
         }
-    
+
 
         const logout = async () => {
             localStorage.removeItem("user")
             await Api.get("/auth/logout")
-            // document.location.reload(true)
+            document.location.reload(true)
         }
 
         const search = () => {
@@ -154,22 +154,24 @@ class Navbar extends React.Component<MyProps, MyState> {
 
                     <div className="menu menu-down hidden w-full lg:block md:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
                         <div className="relative mx-auto text-gray-600 lg:block hidden">
-                            <input
-                                className="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none"
-                                type="search" defaultValue={getParameterByName("q")|| ""} name="search" placeholder="Search" />
-                            <button type="submit" onClick={() => search()} className="absolute right-0 top-0 mt-3 mr-2">
-                                <svg className="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                                    version="1.1" id="Capa_1" x="0px" y="0px"
-                                    viewBox="0 0 56.966 56.966" width="512px" height="512px">
-                                    <path
-                                        d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-                                </svg>
-                            </button>
+                            <form action={"search?q="+($("[name=search]").val())}>
+                                <input
+                                    className="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none"
+                                    type="search" defaultValue={getParameterByName("q") || ""} name="search" placeholder="Search" />
+                                <button type="submit" onClick={() => search()} className="absolute right-0 top-0 mt-3 mr-2">
+                                    <svg className="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                        version="1.1" id="Capa_1" x="0px" y="0px"
+                                        viewBox="0 0 56.966 56.966" width="512px" height="512px">
+                                        <path
+                                            d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                                    </svg>
+                                </button>
+                            </form>
                         </div>
                         <div className="flex">
                             {(this.state.user?.email) ?
                                 <>
-                                    <a href={'/profile/'+this.state.user?.username} className="block text-md px-4 py-2 rounded text-orange-700 ml-2 font-bold hover:text-white mt-4 hover:bg-orange-700 lg:mt-0">{this.state.user?.username}</a>
+                                    <a href={'/profile/' + this.state.user?.username} className="block text-md px-4 py-2 rounded text-orange-700 ml-2 font-bold hover:text-white mt-4 hover:bg-orange-700 lg:mt-0">{this.state.user?.username}</a>
                                     <button onClick={() => logout()} className="block text-md px-4 ml-2 py-2 rounded text-orange-700 font-bold hover:text-white mt-4 hover:bg-orange-700 lg:mt-0">Logout</button>
                                 </>
                                 :
